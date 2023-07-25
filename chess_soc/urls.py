59d52bf362808cc3a38ffc77daed57709b4c6b73
Eplasbaba <adwaith2241@gmail.com>
2023-07-25 22:35:02 +0800

    remove django, change homepage

--- chess_soc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('events/', views.events, name='events'),
-    path('about/', views.about, name='about'),
-    path('resources/', views.articles, name='resources'),
-]
