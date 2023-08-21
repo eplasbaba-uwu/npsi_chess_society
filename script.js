387cb9c7fb82d795d70d9d765c82e245ca0b6863
Eplasbaba <adwaith2241@gmail.com>
2023-08-21 19:04:53 +0800

    navbar dropdown

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const navEl = document.querySelector('.nav');
-const hamburgerEl = document.querySelector('.hamburger');
-const navItemEls = document.querySelectorAll('.nav__item');
+document.addEventListener('DOMContentLoaded', function () {
+  const navEl = document.querySelector('.nav');
+  const hamburgerEl = document.querySelector('.hamburger');
+  const dropdownTriggerEl = document.querySelector('.nav__item.dropdown');
+  const dropdownMenuEl = document.querySelector('.dropdown-menu');
 
-hamburgerEl.addEventListener('click', () => {
-  navEl.classList.toggle('nav--open');
-  hamburgerEl.classList.toggle('hamburger--open');
-});
+  hamburgerEl.addEventListener('click', (e) => {
+    e.stopPropagation(); // Prevent the click event from reaching the document
+    navEl.classList.toggle('nav--open');
+    hamburgerEl.classList.toggle('hamburger--open');
+  });
 
-navItemEls.forEach(navItemEl => {
-  navItemEl.addEventListener('click', () => {
+  dropdownTriggerEl.addEventListener('click', (e) => {
+    e.stopPropagation(); // Prevent the click event from reaching the document
+    dropdownMenuEl.classList.toggle('dropdown-menu--open');
+  });
+
+  // Close the dropdown when clicking outside of it
+  document.addEventListener('click', () => {
     navEl.classList.remove('nav--open');
     hamburgerEl.classList.remove('hamburger--open');
+    dropdownMenuEl.classList.remove('dropdown-menu--open');
+  });
+
+  // Prevent the dropdown from closing when clicking inside it
+  dropdownTriggerEl.addEventListener('click', (e) => {
+    e.stopPropagation(); // Prevent the click event from reaching the document
   });
 });
-
