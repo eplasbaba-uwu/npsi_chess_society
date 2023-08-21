ecfb9f67a253607df7e45a16aa2ba0daa03bd485
Eplasbaba <adwaith2241@gmail.com>
2023-08-21 19:22:58 +0800

    clear pt2

--- script.js
+++ script.js
@@ -3,27 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
   const hamburgerEl = document.querySelector('.hamburger');
   const dropdownTriggerEl = document.querySelector('.nav__item.dropdown');
   const dropdownMenuEl = document.querySelector('.dropdown-menu');
-
   hamburgerEl.addEventListener('click', (e) => {
-    e.stopPropagation(); // Prevent the click event from reaching the document
+    e.stopPropagation();
     navEl.classList.toggle('nav--open');
     hamburgerEl.classList.toggle('hamburger--open');
   });
-
   dropdownTriggerEl.addEventListener('click', (e) => {
-    e.stopPropagation(); // Prevent the click event from reaching the document
+    e.stopPropagation();
     dropdownMenuEl.classList.toggle('dropdown-menu--open');
   });
-
-  // Close the dropdown when clicking outside of it
   document.addEventListener('click', () => {
     navEl.classList.remove('nav--open');
     hamburgerEl.classList.remove('hamburger--open');
     dropdownMenuEl.classList.remove('dropdown-menu--open');
   });
-
-  // Prevent the dropdown from closing when clicking inside it
   dropdownTriggerEl.addEventListener('click', (e) => {
-    e.stopPropagation(); // Prevent the click event from reaching the document
+    e.stopPropagation();
   });
 });
